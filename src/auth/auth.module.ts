ce8d704781fe70ea4961426fb023d09cd4909f10
italo041 <italo_fernando11@hotmail.com>
2022-02-26 22:41:33 -0500

    add strategy at auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -8,6 +8,7 @@ import { AuthService } from './services/auth.service';
 import { AuthController } from './controllers/auth.controller';
 import { UsersRepository } from './repositories/user.repository';
 import config from './../config';
+import { JwtStrategy } from './strategies/jwt-strategy';
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
@@ -25,7 +26,8 @@ import config from './../config';
     }),
     TypeOrmModule.forFeature([UsersRepository]),
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
+  exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule {}

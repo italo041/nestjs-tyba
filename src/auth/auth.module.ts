51c10a7242b88db3124594b38483498f0edb7d90
italo041 <italo_fernando11@hotmail.com>
2022-02-26 21:02:42 -0500

    users repository

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './services/auth.service';
 import { AuthController } from './controllers/auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersRepository } from './repositories/user.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UsersRepository])],
   providers: [AuthService],
   controllers: [AuthController],
 })
